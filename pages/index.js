c5098b6c0efff7ee1795ee58d45a2cf717df8b9a
mima <mima@computas.com>
2022-10-06 12:35:51 +0200

    converted to SSR

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,17 @@ import Head from 'next/head'
 import Link from "next/link";
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-    const [pokemon, setPokemon] = useState([]);
+export async function getServerSideProps(){
+    const response = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
 
-    useEffect(() => {
-        async function getPokemon() {
-            const response = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
-            setPokemon(await response.json());
+    return {
+        props: {
+            pokemon: await response.json(),
         }
+    }
+}
 
-        getPokemon();
-    }, []);
-
+export default function Home({pokemon}) {
     return (
         <>
             <Head>
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,31 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import {useRouter} from "next/router";
-import React, {useState, useEffect} from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styles from '../../styles/Details.module.css'
 
-export default function Details() {
+export async function getServerSideProps({ params }){
+    const response = await fetch(
+        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`);
 
-    const {
-        query: {id},
-    } = useRouter();
-
-    const [pokemon, setPokemon] = useState(null);
-
-    useEffect(() => {
-        async function getPokemon() {
-            const response = await fetch(
-                `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
-            )
-            setPokemon(await response.json());
+    return {
+        props: {
+            pokemon: await response.json(),
         }
+    }
+}
 
-        if (id) {
-            getPokemon();
-        }
-    }, [id]);
-
+export default function Details({pokemon}) {
     if (!pokemon) {
         return null;
     }
